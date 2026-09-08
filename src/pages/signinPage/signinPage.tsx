1850eda0f8e9e794bb5069f7829103681ea70149
kianfarzaneh <kianfarzaneh1380@gmail.com>
2026-09-08 19:03:45 +0330

    fix: permanently remove unused auth pages

--- src/pages/signinPage/signinPage.tsx
+++
@@ -1,9 +0,0 @@
-import Signin from "../../components/signin/signin";
-
-export default function SigninPage() {
-  return (
-    <div className="bg-[#19191a] w-full h-screen flex justify-center items-center">
-      <Signin />
-    </div>
-  );
-}

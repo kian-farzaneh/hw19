6de85c407649cf8dfd713b7256cf7fc9a8aac416
kianfarzaneh <kianfarzaneh1380@gmail.com>
2026-09-08 18:53:10 +0330

    fix : delete unused comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
     <>
       <Router>
         <Routes>
-          {/* <Route path="/" element={<HomePage />} />
-          <Route path="/signin_page" element={<SigninPage />} />
-          <Route path="/signup_page" element={<SignupPage />} /> */}
           <Route path="/" element={<WeatherPage />} />
         </Routes>
       </Router>
